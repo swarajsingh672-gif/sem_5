4c7408bf647b9eefba12007aca5c8db7f2bc968e
swarajsingh672-gif <swarajsingh672@gmail.com>
2025-08-30 21:29:53 +0530

    first commit

---
+++ MostWater.java
@@ -0,0 +1,30 @@
+class MostWater {
+    public int maxArea(int[] height) {
+        int left = 0;                    // start pointer
+        int right = height.length - 1;   // end pointer
+        int maxArea = 0;
+
+        while (left < right) {
+            // Find current area
+            int h = Math.min(height[left], height[right]);
+            int width = right - left;
+            int area = h * width;
+            maxArea = Math.max(maxArea, area);
+
+            // Move the pointer with the smaller height
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+
+    // For testing
+    public static void main(String[] args) {
+        MostWater obj = new MostWater();
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        System.out.println(obj.maxArea(height)); // Output: 49
+    }
+}
